3fd42c2c8c34c913797677cf8a41d8f1bb1d71ac
Yordan Vuchev <danivu4e@abv.bg>
2022-06-18 19:16:38 +0300

    practice

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -5,8 +5,6 @@
 // addToCart('water', 10);
 // console.log(a, b);
 
-// import add from './shoppingCart.js';
-
 // add('coca cola', 15);
 
 /////////////////////////////
@@ -17,17 +15,45 @@
 // const data = await response.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await response.json();
-  console.log(data);
+// const getLastPost = async function () {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await response.json();
+//   console.log(data);
 
-  return {
-    title: data.at(-1).title,
-    text: data.at(-1).body,
-  };
-};
+//   return {
+//     title: data.at(-1).title,
+//     text: data.at(-1).body,
+//   };
+// };
 
-const post = await getLastPost();
+// const post = await getLastPost();
 
-console.log(post);
+// console.log(post);
+
+/*////////////////////////////
+    THE MODULE PATTERN
+////////////////////////////*/
+// const ShoppingCart2(function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`Added ${quantity} ${product} to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered`);
+//   };
+
+//   return addToCart, cart, totalPrice, totalQuantity;
+// })();
+
+import add from './shoppingCart.js';
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+if (module.hot) {
+  module.hot.accept();
+}
